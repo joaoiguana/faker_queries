14eb616c57486d432b284a335dbf0d5aab998cc9
Joao Iguana <jiboiadamor@gmail.com>
2024-06-14 18:47:20 +0100

    added 100 users with random post numbers for each to seeds with faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,26 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'faker'
+
+User.destroy_all
+Post.destroy_all
+
+# Create 100 users
+100.times do
+  user = User.create(
+    name: Faker::Name.name,
+    email: Faker::Internet.email
+  )
+
+  # Create between 1 and 10 posts for each user
+  rand(1..10).times do
+    Post.create(
+      title: Faker::Lorem.sentence,
+      body: Faker::Lorem.paragraph,
+      user: user
+    )
+  end
+end
+
+puts "Seeded #{User.count} users and #{Post.count} posts."
