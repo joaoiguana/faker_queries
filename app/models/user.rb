6b980fb7fec93048ab374c2c42b35d2cbfa1ef9e
Joao Iguana <jiboiadamor@gmail.com>
2024-06-17 13:40:05 +0100

    created class method to display top 5 users with most posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ApplicationRecord
   has_many :posts
+
+  def self.top_5_users
+    User.joins(:posts)
+        .select('users.*, COUNT(posts.id) as posts_count')
+        .group('users.id')
+        .order('posts_count DESC')
+        .limit(5)
+        .includes(:posts)
+  end
 end
